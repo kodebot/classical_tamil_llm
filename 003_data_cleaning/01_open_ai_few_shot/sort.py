c126319f65ea70faeb286b54bc3d37083ee210e4
kodebot <kodebot@hotmail.com>
2025-01-03 22:45:22 +0000

    more cleanup

--- 003_data_cleaning/01_open_ai_few_shot/sort.py
+++ 003_data_cleaning/01_open_ai_few_shot/sort.py
@@ -1,12 +1,12 @@
 import json
 
 # Load the JSON data
-with open('purananuru_cleaned_attempt_2.json', 'r', encoding='utf-8') as file:
+with open('aganaanuru_cleaned_attempt_2.json', 'r', encoding='utf-8') as file:
     data = json.load(file)
 
 # Sort the data by the poem_number field
 poems = sorted(data, key=lambda x: x['poem_number'])
 
 # Save the sorted data back to the JSON file
-with open('puranaanuru_sorted_attempt_3.json', 'w', encoding='utf-8') as file:
+with open('aganaanuru_sorted_attempt_3.json', 'w', encoding='utf-8') as file:
     json.dump(poems, file, ensure_ascii=False, indent=4)
