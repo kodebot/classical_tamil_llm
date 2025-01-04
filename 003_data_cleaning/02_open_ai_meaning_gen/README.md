7df614154b35804c461416040f70ccef6418fa29
kodebot <kodebot@hotmail.com>
2025-01-04 09:37:25 +0000

    more cleanup

---
+++ 003_data_cleaning/02_open_ai_meaning_gen/README.md
@@ -0,0 +1,10 @@
+# OpenAI for upsampling 
+
+## Setup
+
+```bash
+conda install conda-forge::openai
+conda install conda-forge::python-dotenv
+conda install conda-forge::pydantic
+
+```
--- 003_data_cleaning/02_open_ai_meaning_gen/just_ask.py
+++ 003_data_cleaning/02_open_ai_meaning_gen/just_ask.py
@@ -7,36 +7,42 @@ import json
 
 load_dotenv()
 
+class PoemUpSample(BaseModel):
+    poem_number: int
+    poem_meaning: list[str]
+
 # Initialize OpenAI API client
 openai_api_key = os.getenv("OPENAI_API_KEY")
 client = OpenAI(api_key=openai_api_key)
 
-def reformat_poem():
+def get_meaning(poem):
     completion = client.beta.chat.completions.parse(
         model="gpt-4o",
         messages=[
-            {"role": "system", "content": "You are a helpful Tamil classic literature and poems expert assistant who is helping a user to clean up a text document in Tamil language."},
+            {"role": "system", "content": "You are a helpful Tamil classic literature and poems expert assistant who is helping a user to get the meaning of Tamil classical poems in modern Tamil."},
             { 
                 "role": "user", 
-                "content": f"""Give me Purananuru poem 6 with title, poem and its meaning in Tamil in JSON format with title, poem and meaning fields. """
+                "content": f"""Provide a very detailed and descriptive meaning of this poem - {poem}"""
             },
         ],
+        response_format=PoemUpSample
     )
     return completion.choices[0].message
 
 
 # Example usage
 if __name__ == "__main__":
-    print(reformat_poem().content)
 
-    # with open('aganaanuru_text.json', 'r', encoding='utf-8') as file:
-    #     data = json.load(file)
+    with open('../../004_datasets/Purananuru/puranaanuru_web.json', 'r', encoding='utf-8') as file:
+        data = json.load(file)
 
-    # # Sort the data by the title field
-    # cleaned_data = []
-    # for item in data:
-    #     cleaned_data.append(clean_text(item).content)
-    #     print(f"cleaned {len(cleaned_data)} of {len(data)}\n")
-    #     with open('aganaanuru_cleaned.json', 'w', encoding='utf-8') as f:
-    #         json.dump(cleaned_data, f, ensure_ascii=False, indent=4)
+    upsampled = []
+    for item in data:
+        input = f""""poem_number":{item["poem_number"]}, "poem_content": {item["poem_content"]}"""
+        upsampled.append(get_meaning(input).content)
+        print(f"cleaned {len(upsampled)} of {len(data)}\n")
+        with open('purananuru_upsample.json', 'w', encoding='utf-8') as f:
+            json.dump(upsampled, f, ensure_ascii=False, indent=4)
+
+
 
