dfc5dca09a970fb65233abd02744cab3bc54438d
kodebot <kodebot@hotmail.com>
2025-01-17 22:19:53 +0000

    wip

--- 003_data_cleaning/03_LLaMA_prep/llama_dataset_prep.py
+++ 003_data_cleaning/03_LLaMA_prep/llama_dataset_prep.py
@@ -18,14 +18,18 @@ if __name__ == "__main__":
 
 
     result = []
-    with open('../../004_datasets/Purananuru/puranaanuru_web.json', 'r', encoding='utf-8') as file:
-        data = json.load(file)
-        for line in data:
-            poem_number = f"இது புறநானூறு பாடல் {line["poem_number"]} "
-            poem_meaning = f"இந்த பாடலின் பொருள்: {" ".join(line["poem_meaning"])}"
-            poem_context = f"இந்த பாடல் சம்பந்தமான சில தகவல்கள்: {" ".join(line["poem_context"])}"
-            poem = " ".join(line["poem_content"])
-            result.append( f"""<s>[INST]{poem}[/INST]{poem_number} {poem_meaning} {poem_context}</s>\n""")
+    with open('aganaanuru_web.json', 'r', encoding='utf-8') as file1:
+        data1 = json.load(file1)
+        with open('aganaanuru_upsample.json', 'r', encoding='utf-8') as file:
+            data = json.load(file)
+            for line in data:
+                line1 = [x for x in data1 if x["poem_number"] == line["poem_number"] ][0]
+                print(line1)
+                poem_number = f"இது அகநானூறு பாடல்் {line["poem_number"]} "
+                poem_meaning = f"இந்த பாடலின் பொருள்: {" ".join(line["poem_meaning"])}"
+                poem_context = f"இந்த பாடல் சம்பந்தமான சில தகவல்கள்: {" ".join(line1["poem_context"])}"
+                poem = " ".join(line1["poem_content"])
+                result.append( f"""<s>[INST]{poem}[/INST]{poem_number} {poem_meaning} {poem_context}</s>\n""")
 
-    with open('purananuru_llama_inst.txt', 'w', encoding='utf-8') as f:
-        f.writelines(result)
+        with open('aganaanuru_llama_inst_upsampled.txt', 'w', encoding='utf-8') as f:
+            f.writelines(result)
